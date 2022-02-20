1f6c3a806d486e6f0ace4a293763bc54e77fba52
RoksolanaMazurak <roksamazurak@gmail.com>
2022-02-20 23:41:07 +0200

    lab-01-java

---
+++ .idea/src/ua/lviv/iot/Jemstone.java
@@ -0,0 +1,37 @@
+package ua.lviv.iot;
+
+public record Jemstone (String name, float number_of_carats, float price,
+                        float density, String color, String opacity) {
+
+    public static String shape = "square";
+
+    public Jemstone() {
+        this("unknown",0,0,0,"unknown", "unknown");
+    }
+
+    public Jemstone(String name, float number_of_carats, float price) {
+        this(name, number_of_carats, price, 0, "unknown", "unknown");
+
+    }
+
+    public Jemstone(String name, float number_of_carats, float price,
+                    float density, String color, String opacity) {
+        this.name = name;
+        this.number_of_carats = number_of_carats;
+        this.price = price;
+        this.density = density;
+        this.color = color;
+        this.opacity = opacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Name of jemstone: " + name + "," + " number of carats: " + number_of_carats + ","
+                + " price: " + price + "," + " density: " + density + "," + " color: " + color + ","
+                + " opacity: " + opacity + ".";
+    }
+
+    public static String returnShape() {
+        return "The shape of jemstones is: " + shape + ".";
+    }
+}
---
+++ .idea/src/ua/lviv/iot/Main.java
@@ -0,0 +1,22 @@
+package ua.lviv.iot;
+
+import ua.lviv.iot.Jemstone;
+
+public class Main {
+    public static void main(String[] args) {
+        Jemstone defaultStone = new Jemstone();
+        Jemstone diamond = new Jemstone("diamond", 1, 6500);
+        Jemstone sapphire = new Jemstone("sapphire", 3, 9300, 9, "green","transparent");
+
+        System.out.println(defaultStone.toString());
+        System.out.println(diamond.toString());
+        System.out.println(sapphire.toString());
+
+        System.out.println();
+
+        System.out.println(defaultStone.returnShape());
+
+    }
+
+
+}
